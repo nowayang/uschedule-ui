c87edad6cd76a4ce917561be91ec2852722a6e4f
kamil.nowak <kamil.nowak@sita.aero>
2023-05-07 15:07:07 +0200

    Added envs

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  appUrl: 'http://localhost:4200',
+  apiUrl: 'http://localhost:8080'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  appUrl: undefined,
+  apiUrl: undefined
+};
